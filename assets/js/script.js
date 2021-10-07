47ce8839bb0ea1765f3033d10923363a05ff8028
shnzw124 <shnzw124.com@gmail.com>
2021-10-07 19:00:45 +0900

    タブ切り替え実装

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,19 @@ $(function(){
     $('.burger__wrapper').fadeToggle(500);
   });
 
+  // タブ
+  $('.news__tabs-link').click(function(){
+    if($(this).hasClass("active")){
+      return false;
+    } else {
+      $('.news__content').hide();
+      $($(this).attr("href")).show();
+      $('.news__tabs-link').removeClass("active");
+      $(this).addClass("active");
+      return false;
+    }
+  });
+
   // AOS.js
   AOS.init({
     duration: 3000
