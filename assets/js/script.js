f028dacb9d34cef82f2acd3e5cb360e04ddfb35e
shnzw124 <shnzw124.com@gmail.com>
2021-10-07 19:44:15 +0900

    ファーストビューアニメーション実装

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,6 +21,19 @@ $(function(){
     }
   });
 
+  // ファーストビューアニメーション
+  var mySwiper = new Swiper(".swiper-container", {
+    // Optional parameters
+    loop: true, // ループの指定
+    effect: "fade", //フェードの指定
+    autoplay: {
+        delay: 4000, //４秒後に次のスライドへ
+        disableOnInteraction: false //ユーザー側で操作してもスライドを止めない
+    },
+    speed: 2000, //２秒かけてフェードで切り替わる
+    allowTouchMove: false,//マウスでのスワイプを禁止
+  });
+
   // AOS.js
   AOS.init({
     duration: 3000
